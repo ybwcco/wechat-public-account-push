33bf17324f71fceff215d9f5a9f3ce85cf24a31a
ybwcco <52005816+ybwcco@users.noreply.github.com>
2022-11-11 16:43:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -26,6 +26,9 @@ const USER_CONFIG = {
         {
           type: '节日', name: '相爱纪念日', year: '2019', date: '01-26',
         },
+        {
+          type: '节日', name: '考研日', year: '2022', date: '12-24',
+        },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
@@ -33,8 +36,6 @@ const USER_CONFIG = {
         { keyword: 'know_day', date: '2019-01-23' },
         // 在一起的日子
         { keyword: 'love_day', date: '2019-01-26' },
-        // 考研的日子
-        { keyword: 'text_day', date: '2019-01-26' },
       ],
     },
   ],
